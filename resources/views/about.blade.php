096ff5f5df0c926f96767eb355c798d26a40a6b5
Jonas Christensen <jonasmoeller@gmail.com>
2019-05-07 20:22:16 +0200

    updated about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,20 @@
-@extends('layouts.app')
+@extends('layout')
+
+@section('title','About')
+
+@section('content')
+
+	<div class="container">
+
+		<p><strong>signup</strong>fund v.1 
+		<br>for people with the cashflow and desire to invest more actively. But without the time. Signupfund simply runs a live portfolio of trades in the global investment space and is meant to be copied by users of they like the strategy. </p> 
+		<br>
+		<br>
+		<p></p>
+
+
+
+
+	</div>
+
+@endsection
--- resources/views/unauthorized.blade.php
+++ resources/views/unauthorized.blade.php
@@ -2,14 +2,12 @@
 
 @section('title','Unauthorized')
 
-@section('name')
-
-<h1>Hi {{$user->name}}</h1>
-
-@endsection
-
 @section('content')
 
-<h2>Unfortunately you dont have access to this page. Write to X to solve it.</h1>
+	<div class="container">
+
+		<p>Unfortunately you dont have access to this page. Write to X to solve it.</p>
+
+	</div>
 
 @endsection
